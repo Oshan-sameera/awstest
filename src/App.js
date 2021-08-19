ca922f93525d057cb27c54ff4d804c2b05c4315a
oshan-sameera <oshan951@gmail.com>
2021-08-19 13:22:41 +1000

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Oshan Sameera App</h1>
+          <h1>AWS APP</h1>
         </p>
         <a
           className="App-link"
